9bf7d17414c65305d046d52011e9feb3aca3903d
Diego Monroy <difemo91@gmail.com>
2020-02-24 20:56:53 +0000

    [FIX] spaces

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -65,5 +65,5 @@ class FileStorage:
             if obj:
                 key = "{}.{}".format(type(obj).__name__, obj.id)
                 del self.__objects[key]
-            except:
-                pass
+        except:
+            pass
